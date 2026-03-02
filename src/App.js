837243f8803177782505f52c94d666fc4ca48506
Aniket Tirakpadi <161046661+anikettirakpadi6@users.noreply.github.com>
2026-03-02 15:28:43 +0530

    Remove basename from Router component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-d
 export default class App extends Component {
   render() {
     return (
-      <Router basename="/newskoala">
+      <Router>
         <Navbar />
         <Routes>
           <Route path="/" element={<News key="home" pageSize={7} category="general" />} />
